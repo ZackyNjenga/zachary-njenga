6862ccf60f56175e45903db5763299517f9dc46e
zacharynjenga381@gmail.com <zacharynjenga381@gmail.com>
2024-06-07 20:54:35 +0300

    thats the hero section of the ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative bg-black">
-      <div>
-        This is my portfolio, Oh hello.
+    <main className="relative bg-black felx justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+
+      <div className="max-w-7xl w-full">
+        <Hero/>
       </div>
     </main>
   );
